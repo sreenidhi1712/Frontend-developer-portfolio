d0d8c985e0289696dbd9dca22b8bed314dfdd061
[Sreenidhi D] <[sreeindra01@gmail.com]>
2024-06-24 20:03:55 +0530

    card description update

--- src/Mywork/Mywork.jsx
+++ src/Mywork/Mywork.jsx
@@ -3,6 +3,7 @@ import styles from "./Mywork.module.css"
 // import { worksmade } from './work'
 import projectimage from './proj.jpg'
 import chanakya from './Chanakya.png'
+import  Newswing from './Newswing.png' 
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
@@ -27,8 +28,9 @@ function Mywork() {
                                 </div>
                                         
                             </div>
-                            <div className={styles.ProjectName}>
-                            Vibhaa -  E-commerce Website
+                            <div className={styles.ContentContainer}>
+                            <div className={styles.ProjectNameContainer}>
+                            <p className={styles.ProjectName}>Vibhaa -  E-commerce Website</p>
                             </div>
                             <div className={styles.Description}>
                             <p>A Web Application for providing users with a seamless online shopping experience</p>
@@ -37,32 +39,36 @@ function Mywork() {
                                 <button className={styles.Button}><a href='https://github.com/sreenidhi1712/Vibhaa-Ecommerce-website.git' className={styles.projlinks}>View Project</a></button>
                                 <button className={styles.Button}>View Code</button>
                             </div>
+                            </div>
                         </div>
-                        <div className={styles.Cards} data-aos = "fade-up"  data-aos-delay="500">
+                        <div className={styles.Cards} data-aos = "fade-up"  data-aos-delay="200">
                             <div className={styles.ImageHolder}>
                             <div className={styles.Imagepositioner}>
-                                       <img src={projectimage} alt='projecimages' className={styles.ProjectImg}/>
+                                       <img src={Newswing} alt='projecimages' className={styles.ProjectImg}/>
                                 </div>
                             </div>
-                            <div className={styles.ProjectName}>
-                            News-wings
+                            <div className={styles.ContentContainer}>
+                            <div className={styles.ProjectNameContainer}>
+                            <p className={styles.ProjectName}>News-wings</p>
                             </div>
                             <div className={styles.Description}>
-                            <p>A Web Application Designed for automating the hostel room allocation </p>
+                            <p> News-wings, a cutting-edge news app built with React and Tailwind CSS. Stay informed with real-time updates and a personalized news feed tailored just for you.</p>
                             </div>
                             <div className={styles.ButtonContainer}>
                                 <button className={styles.Button}><a href='https://github.com/sreenidhi1712/News-wings.git' className={styles.projlinks}>View Project</a></button>
                                 <button className={styles.Button}>View Code</button>
                             </div>
+                            </div>
                         </div>
-                        <div className={styles.Cards} data-aos = "fade-up"  data-aos-delay="1000">
+                        <div className={styles.Cards} data-aos = "fade-up"  data-aos-delay="500">
                             <div className={styles.ImageHolder}>
                             <div className={styles.Imagepositioner}>
                                        <img src={chanakya} alt='projecimages' className={styles.ProjectImg}/>
                                 </div>
                             </div>
-                            <div className={styles.ProjectName}>
-                            AI Chat Bot
+                            <div className={styles.ContentContainer}>
+                            <div className={styles.ProjectNameContainer}>
+                            <p className={styles.ProjectName}>AI Chat Bot</p>
                             </div>
                             <div className={styles.Description}>
                            <p> An AI-driven Web Application designed to provide users  content generation capabilities, similar to Google's Gemini</p>
@@ -71,7 +77,7 @@ function Mywork() {
                                 <button className={styles.Button}><a href='https://github.com/sreenidhi1712/Chanakya-AI.git' className={styles.projlinks}>View Project</a></button>
                                 <button className={styles.Button}>View Code</button>
                             </div>
-                            
+                            </div>
                         </div>
                   
                 </div>
